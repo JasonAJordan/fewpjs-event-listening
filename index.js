249d7336e50fab573cd999e7d5042033286e7142
JasonAkiraJordan <jasonajordan0@gmail.com>
2020-12-13 20:41:11 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-function addingEventListener() {
-}
+// const input = document.querySelector('input');
+
+// function addingEventListener() {
+//     input.addEventListener('click', function(event) {
+//    alert('I was clicked!');
+// return "clicked"
+// }); 
+// }
+
+const input = document.getElementById('input');
+input.addEventListener('click', function(event) {
+  alert('I was clicked!');
+  return "clicked"
+}); 
